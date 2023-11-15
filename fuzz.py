f95af30486961eafd3b1dab8eba33a3c2ae24335
earpboy1064 <wyatt.lemaster@gmail.com>
2023-11-14 19:57:09 -0600

    added fuzzing file

---
+++ fuzz.py
@@ -0,0 +1,11 @@
+
+
+##count_initial_comment_line (filepath):
+
+##def checkIfWeirdYAML(yaml_script):
+
+##def getKeyRecursively(  dict_, list2hold,  depth_ = 0  ) :
+import parser
+import logger
+
+print(parser.checkIfWeirdYAML("FuzzingFiles/Comment_Line_Test_1.yaml"))
